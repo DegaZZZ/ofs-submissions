0295672efdb1390738ca65da8b3c55a3d46ddf55
DegaZZZ <platon.makinen@aalto.fi>
2023-07-17 20:24:43 +0300

    d

---
+++ part2/2.12-2.15/components/Filter.js
@@ -0,0 +1,10 @@
+const Filter = ({ searchTerm, handleSearch }) => {
+  return (
+    <div>
+      Search:
+      <input value={searchTerm} onChange={handleSearch} />
+    </div>
+  )
+}
+
+export default Filter
---
+++ part2/2.12-2.15/services/persons.js
@@ -0,0 +1,21 @@
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3001/persons'
+
+const getAll = () => {
+  return axios.get(baseUrl)
+}
+
+const create = newObj => {
+  return axios.post(baseUrl, newObj)
+}
+
+const update = (id, newObj) => {
+  return axios.put(`${baseUrl}/${id}`, newObj)
+}
+
+const deletePerson = id => {
+  return axios.delete(`${baseUrl}/${id}`)
+}
+
+export default { getAll, create, update, deletePerson }
